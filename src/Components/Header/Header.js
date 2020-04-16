a9e678cf363002d9cbdc2f50668f5680f087c845
dharmawipraja <dharmawipraja@gmail.com>
2020-04-17 01:13:43 +0700

    change renderError func

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,7 @@ import { useToasts } from 'react-toast-notifications';
 
 import { GlobalContext } from '../../Context/GlobalState';
 
-const renderError = (msg, addToast, removeNotifications) => {
+const renderErrorNotif = (msg, addToast, removeNotifications) => {
   if (msg.length !== 0) {
     // Don't know why but need to remove first so error toast only appear once
     removeNotifications();
@@ -13,17 +13,17 @@ const renderError = (msg, addToast, removeNotifications) => {
   }
 };
 
-const Header = () => {
+function Header() {
   const { error, removeNotifications } = useContext(GlobalContext);
   const { addToast } = useToasts();
   return (
     <>
-      {error && renderError(error, addToast, removeNotifications)}
+      {error && renderErrorNotif(error, addToast, removeNotifications)}
       <h2>
         MONEY TRACKER
       </h2>
     </>
   );
-};
+}
 
 export default Header;
